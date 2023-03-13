d80c106874a7a067efa637ad096eb6d3867d4d74
Shubham Sati <68639424+Shubham-Sati@users.noreply.github.com>
2023-03-13 14:56:48 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Cart from "./pages/Cart/Cart";
 import SignIn from "./pages/SignIn/SignIn";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Products from "./pages/ProductList/Products"
+import Product from "./pages/ProductList/Product";
 import CreateAccount from "./pages/CreateAccount/CreateAccount";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import Checkout from "./pages/Checkout/Checkout";
@@ -20,7 +20,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
+          <Route path="/products" element={<Product />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/signIn" element={<SignIn />} />
---
+++ src/pages/ProductList/Product.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Product.css";
+
+const Product = () => {
+  return <div className="product">Here are Products</div>;
+};
+
+export default Product;
