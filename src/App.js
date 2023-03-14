ddb12669d00b24b92cabeef0fc32513a810884f3
Shubham Sati <68639424+Shubham-Sati@users.noreply.github.com>
2023-03-14 12:42:36 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.js";
+// import Home from "./pages/Home/Home.js";
+import Home from "./pages/Home/Home"
 import Contact from "./pages/Contact/Contact";
 import Cart from "./pages/Cart/Cart";
 import SignIn from "./pages/SignIn/SignIn";
@@ -29,7 +30,7 @@ function App() {
             <Route path="forgotPassword" element={<ForgotPassword />} />
             <Route path="checkout" element={<Checkout />} />
             <Route path="products/:id" element={<ProductDetail />} />
-            <Route path="admin"> 
+            <Route path="admin">
               <Route
                 path="products"
                 element={<Dashboard currentPage="products" />}
