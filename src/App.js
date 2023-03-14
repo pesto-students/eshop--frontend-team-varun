1b58052e46a42943e95ebf284117f78c137aceb6
Shubham Sati <68639424+Shubham-Sati@users.noreply.github.com>
2023-03-14 12:47:38 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home/Home.js";
-import Home from "./pages/Home/Home"
+import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
 import Cart from "./pages/Cart/Cart";
 import SignIn from "./pages/SignIn/SignIn";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Product from "./pages/ProductList/Product";
+import Product from "./pages/Product/Product";
 import CreateAccount from "./pages/CreateAccount/CreateAccount";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import Checkout from "./pages/Checkout/Checkout";
--- src/pages/ProductList/Product.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Product.css";
-
-const Product = () => {
-  return <div className="product">Here are Products</div>;
-};
-
-export default Product;
